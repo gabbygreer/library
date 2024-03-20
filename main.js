8110d40f98052f9571bec92d02ab0f8ce7b6db5a
soupsmagilly <136518804+soupsmagilly@users.noreply.github.com>
2024-03-20 18:55:52 -0500

    App is working. Reorganized main.js by rearranging and add a few functions to get the program working better. Added a close button.

--- main.js
+++ main.js
@@ -1,31 +1,10 @@
 //  VARIABLES
-
-const button = document.getElementById('popup-button');
+const myLibrary = [];
+const newBookBtn = document.getElementById('popup-button');
 const formWindow = document.getElementById('popup-div');
 const form = document.getElementById('popup-form')
-const myLibrary = [];
 
-//  EVENT LISTENERS
-form.addEventListener('submit', addBookToLibrary) 
-
-
-//Popup form start
-button.addEventListener('click', function() {
-    // form.style.display = 'flex';
-    formWindow.classList.toggle('form-open')
-    formWindow.classList.toggle('form-closed')
-});
-
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    // form.style.display = 'none';
-    formWindow.classList.toggle('form-open')
-    formWindow.classList.toggle('form-closed')
-});
-// popup form end
-
-
-//  book constructor function
+//  Book constructor function
 function Book(title, author, numOfPages, haveRead) {
     this.title = title;
     this.author = author;
@@ -36,58 +15,81 @@ function Book(title, author, numOfPages, haveRead) {
     }
 }
 
-
-
-// //container to hold book cards
-// const container = document.querySelector('.card-container');
-
-// //renders books from myLibrary to the DOM
-// function renderLibrary() {
-//     //loops through every element in array
-//     myLibrary.forEach(book => {
-//         //creating new elements and assigning them to variables
-//         const div = document.createElement('div');
-//         const bookTitle = document.createElement('h2');
-//         const authorName = document.createElement('h4');
-//         const pageCount = document.createElement('h4')
-//         const alreadyRead = document.createElement('h4')
-
-//         //adding class to newly created div container so it can be styled
-//         div.classList = 'card'
-
-//         //populating the new elements with the property values from the objects in the myLibrary array
-//         bookTitle.innerText = `${book.title}`;
-//         authorName.innerText = `${book.author}`;
-//         pageCount.innerText = `${book.numOfPages}`;
-//         alreadyRead.innerText = `${book.haveRead}`
-
-//         //append these new elements to the new div element
-//         div.appendChild(bookTitle);
-//         div.appendChild(authorName);
-//         div.appendChild(pageCount);
-//         div.appendChild(alreadyRead);
-//         //append the div element to the DOM
-//         container.appendChild(div)
-//     }) 
-// };
-
-
-//  pulls user input and creates a new Book and adds it to myLibrary array
-function addBookToLibrary(event) {
-    //prevents default form submit
-    event.preventDefault();
-
-    //assigning variables to the values from user input
-    const title = document.getElementById('title').value;
-    const author = document.getElementById('author').value;
-    const numOfPages = document.getElementById('numOfPages').value;
-    const haveRead = document.getElementById('haveRead').value;
-
-    //createing a new object
-    const newBook = new Book(title, author, numOfPages, haveRead)
-    //adds new book to myLibrary array
-    return myLibrary.push(newBook);
+//adds book object to myLibrary array
+function addBookToLibrary(book) {
+    myLibrary.push(book)
 }
 
-// const book1 = new Book('Broken Harbor', 'French, Tana', 450, "Read")
-// const book2 = new Book('Song of Solomon', 'Morrison, Toni', 337, "Read")
+//renders books from myLibrary to the DOM
+function renderBook(book) {
+    const container = document.querySelector('.card-container'); //container to hold book cards
+
+    //creating new elements and assigning them to variables
+    const div = document.createElement('div');
+    //adding class to newly created div container so it can be styled
+    div.classList = 'card'
+    const bookTitle = document.createElement('h2');
+    const authorName = document.createElement('h4');
+    const pageCount = document.createElement('h4')
+    const alreadyRead = document.createElement('h4')
+    
+    //populating the new elements with the property values from the objects in the myLibrary array
+    bookTitle.innerText = `${book.title}`;
+    authorName.innerText = `${book.author}`;
+    pageCount.innerText = `${book.numOfPages}`;
+    alreadyRead.innerText = `${book.haveRead}`;
+     
+    //append these new elements to the new div element
+    div.appendChild(bookTitle);
+    div.appendChild(authorName);
+    div.appendChild(pageCount);
+    div.appendChild(alreadyRead);
+    //append the div element to the DOM
+    container.appendChild(div)
+};
+
+
+function renderLibrary(book) {  //  Loops through myLibrary and runs renderBook function on each book
+    for (book of myLibrary) {
+        renderBook(book)
+    }
+}
+
+const brokenHarbor = new Book('Broken Harbor', 'French, Tana', 450, "Read");
+const songOfSolomon = new Book('Song of Solomon', 'Morrison, Toni', 337, "Read");
+addBookToLibrary(brokenHarbor);
+addBookToLibrary(songOfSolomon);
+
+renderLibrary();
+
+
+//  SUBMIT EVENT LISTENER
+form.addEventListener('submit', (e) => {
+    const newBook = new Book(document.querySelector('#title').value, 
+                            document.querySelector('#author').value,
+                            document.querySelector('#numOfPages').value, 
+                            document.querySelector('#haveRead').value);
+        
+        myLibrary.push(newBook)
+        form.reset();
+        //prevents default form submit
+        e.preventDefault();  
+        renderBook(newBook)                   
+}) 
+
+//Popup form start
+newBookBtn.addEventListener('click', function() {
+    formWindow.classList.toggle('form-open')
+    formWindow.classList.toggle('form-closed')
+});
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    formWindow.classList.toggle('form-open')
+    formWindow.classList.toggle('form-closed')
+});
+
+form.addEventListener('reset', function(){
+    formWindow.classList.toggle('form-open')
+    formWindow.classList.toggle('form-closed')
+})
