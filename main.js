6ecb09344eaf21aff6264c7a6d7b8b2fb7674eb5
soupsmagilly <136518804+soupsmagilly@users.noreply.github.com>
2024-03-22 22:41:42 -0500

    Refactored some things and got the checkbox on the form to determine read status on the object. Still need to get the read button working with the toggleRead function.

--- main.js
+++ main.js
@@ -1,136 +1,89 @@
-//  VARIABLES
 const myLibrary = [];
-const library = document.querySelector('.card-container')
-const form = document.getElementById('popup-form')
-const newBookModal = document.querySelector('#modal')
-const openModal = document.querySelector('#open-button');
-const closeModal = document.querySelector('#close-button');
+const form = document.querySelector('#popup-form')
+const openModal = document.querySelector('#open-button').addEventListener('click', () => {modal.showModal();});
+const closeModal = document.querySelector('#close-button').addEventListener('click', () => {modal.close();
+                                                                                            form.reset()});
 
-//  Book constructor function
-function Book(title, author, numOfPages, haveRead, bookIndex) {
-    this.title = title;
-    this.author = author;
-    this.numOfPages = numOfPages;
-    this.haveRead = haveRead;
-    this.bookIndex = bookIndex;
-    this.info = function info() {
-        return `${title} by ${author}, ${numOfPages}, ${haveRead}`
+
+class Book {
+    constructor(title, author, numOfPages, read){
+        this.title = title,
+        this.author = author,
+        this.numOfPages = numOfPages,
+        this.read = read;
+    }
+    toggleRead(book) {
+        this.read = !this.read
     }
 }
 
-//adds book object to myLibrary array
-function addBookToLibrary(book) {
-    myLibrary.push(book)
-}
-
-//renders books from myLibrary to the DOM
-function renderBook(book) {
-    const container = document.querySelector('.card-container'); //container to hold book cards
-
-    const bookDiv = document.createElement('div'); //creating card div that will hold book info
-    bookDiv.classList = 'book-card'; //adding class to newly created div container so it can be styled
-    container.appendChild(bookDiv); //append the div element to the DOM
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const numOfPages = document.querySelector('#numOfPages');
+const read = document.querySelector('#bookStatus');
     
-    const bookTitle = document.createElement('h2'); //creating new elements and assigning them to variables
-    bookTitle.innerText = `${book.title}`; //populating the elements with the values from myLibrary
-    bookDiv.appendChild(bookTitle); //append new element to the new div element
+function addBookToLibrary(book) {
+    const newBook = new Book(title.value, author.value, numOfPages.value, read.checked)
+    myLibrary.push(newBook)
+}
+    
+function renderBookCard(book) {
 
-    const authorName = document.createElement('h4');
-    authorName.innerText = `${book.author}`;
-    bookDiv.appendChild(authorName);
+        const container = document.querySelector('.card-container');
 
-    const pageCount = document.createElement('h4');
-    pageCount.classList = 'numOfPages'
-    pageCount.innerText = `${book.numOfPages} pages`;
-    bookDiv.appendChild(pageCount);
+        const bookCard = document.createElement('div');
+        bookCard.classList = 'book-card';
+        container.appendChild(bookCard); 
+        
+        const bookTitle = document.createElement('h2');
+        bookCard.appendChild(bookTitle);
+        bookTitle.textContent = title.value; 
 
-    //  BOOK ACTIONS
-    const actionContainer = document.createElement('div');
-    actionContainer.classList = 'card-action-container';
-    bookDiv.appendChild(actionContainer);
+        const authorName = document.createElement('h4');
+        bookCard.appendChild(authorName); 
+        authorName.textContent = author.value;
 
-    //  DROPDOWN
-    const bookReadContainer = document.createElement('div');
-    bookReadContainer.classList = 'book-read-container'
-    actionContainer.appendChild(bookReadContainer);
+        const pageCount = document.createElement('h5');
+        bookCard.appendChild(pageCount); 
+        pageCount.textContent = numOfPages.value; 
 
-    const readLabel = document.createElement('label');
-    readLabel.classList = 'read-label'
-    readLabel.for = 'haveRead';
-    readLabel.innerText = 'Read?';
-    bookReadContainer.appendChild(readLabel);
+        const buttonContainer = document.createElement('div');
+        buttonContainer.classList = 'card-actions';
+        bookCard.appendChild(buttonContainer);
 
-    const readStatus = document.createElement('select');
-    readStatus.classList = 'checkbox'
-    readStatus.name = 'bookReadStatus';
-    readStatus.id = 'haveRead';
-    bookReadContainer.appendChild(readStatus);
+        const readStatusButton = document.createElement('a');
+        readStatusButton.class = 'read-status';
+        buttonContainer.appendChild(readStatusButton);
+        if(read === false){
+            readStatusButton.textContent = 'Not Read';
+            readStatusButton.classList = 'not-read';
+        }else {
+            readStatusButton.textContent = 'Read';
+            readStatusButton.classList = 'read'
+        }
 
-    const readOption = document.createElement('option');
-    readOption.innerText = 'Read'
-    readOption.value = 'read';
-    readStatus.appendChild(readOption);
+        const deleteBookButton = document.createElement('a');
+        deleteBookButton.class = 'delete-button';
+        buttonContainer.appendChild(deleteBookButton);
+        deleteBookButton.textContent = "DELETE";
 
-    const unreadOption = document.createElement('option');
-    unreadOption.innerText = 'To Be Read'
-    unreadOption.value = 'unread';
-    readStatus.appendChild(unreadOption);
-
-
-
-    //
-
-    //  DELETE BUTTON   
-    const deleteBook = document.createElement('a');
-    actionContainer.appendChild(deleteBook);
-
-    const trashIcon = document.createElement('img');
-    trashIcon.classList = 'trash'
-    trashIcon.src = 'images/trash-icon.svg'
-    deleteBook.appendChild(trashIcon);
-
-    deleteBook.addEventListener('click', () => {
-        let bookIndex = myLibrary.indexOf(book);
-        myLibrary.splice(bookIndex, 1);
-        library.removeChild(library.children[bookIndex])
-    })
-    //
+        // //  associates the book object and the book card
+        // bookCard.dataset.index = myLibrary.indexOf(book)
+       
+        deleteBookButton.addEventListener('click', () => {
+            myLibrary.splice(myLibrary.indexOf(book), 1);
+            bookCard.remove();
+        });    
 };
 
 
-function renderLibrary(book) {  //  Loops through myLibrary and runs renderBook function on each book
-    for (book of myLibrary) {
-        renderBook(book)
-    }
-}
-
-const brokenHarbor = new Book('Broken Harbor', 'Tana French', 450, "Read", 1);
-const songOfSolomon = new Book('Song of Solomon', 'Toni Morrison', 337, "Read", 2);
-addBookToLibrary(brokenHarbor);
-addBookToLibrary(songOfSolomon);
-
-renderLibrary();
-
-//  POPUP FORM
-openModal.addEventListener('click', () => {
-    modal.showModal();
-});
-closeModal.addEventListener('click', () => {
-    modal.close();
-});
-
-//  SUBMIT EVENT LISTENER
 form.addEventListener('submit', (e) => {
-    const newBook = new Book(document.querySelector('#title').value, 
-                            document.querySelector('#author').value,
-                            document.querySelector('#numOfPages').value, 
-                            document.querySelector('#haveRead').value,
-                            document.querySelectorAll('.book-card').length + 1);
-
-        myLibrary.push(newBook)
-        e.preventDefault();  //prevents default form submit
-        renderBook(newBook);
-        modal.close();      
-        form.reset();         
+    e.preventDefault();  
+    addBookToLibrary();
+    renderBookCard();
+    modal.close();
+    form.reset();       
 }) 
 
+let emma = new Book('Emma', 'Jane Austen', 450, true);
+myLibrary.push(emma);
